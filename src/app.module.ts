ec6072dd8f743a3b97bdcd5c0b09a20045e89839
Andrey <kan@seo-gravity.ru>
2025-05-28 23:38:30 +0300

    первые настройки post запроса

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { TranslateModule } from './translate/translate.module';
 import { ProjectModule } from './project/project.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TranslateModule } from './translate/translate.module';
+
 
 @Module({
   imports: [
@@ -14,7 +14,7 @@ import { TranslateModule } from './translate/translate.module';
       useFactory: () => ({
         type: 'postgres',
         host: process.env.DB_HOST,
-        port: +process.env.DB_PORT,
+        port: +(process.env.DB_PORT || 5432),
         username: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME,
